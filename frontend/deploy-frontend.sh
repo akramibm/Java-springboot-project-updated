db4b09bc18e6ffa7c8b88fdc49e8797cb3da441b
akramibm <akramafridi02@gmail.com>
2026-09-16 00:28:39 +0530

    fix: extract deploy script to separate file to eliminate YAML parser error

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -11,16 +11,16 @@ listen [::]:80 default_server;
 
 location / {
     proxy_pass http://127.0.0.1:5000;
-    proxy_set_header Host $host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 }
 
 location /api/ {
     proxy_pass http://127.0.0.1:8080/;
-    proxy_set_header Host $host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 }
 }
 NGINX
@@ -28,7 +28,7 @@ NGINX
 nginx -t
 systemctl restart nginx
 
-# Setup Python virtual environment & dependencies
+# Setup Python venv and dependencies
 cd /opt/frontend
 if [ ! -d venv ]; then
 python3 -m venv venv
